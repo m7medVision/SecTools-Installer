a14923b2c46b652216e98579b9c658e3fc3a1606
Mohammed Al-Jahwari <88824957+majhcc@users.noreply.github.com>
2022-07-14 13:39:06 +0400

    fix

--- m.bat
+++ m.bat
@@ -2,4 +2,4 @@ git clone https://github.com/s0md3v/XSStrike.git %userprofile%\desktop\tools\XSS
 echo "Downlloading DangerousZone Pack...."
 git clone https://github.com/malwares/DangerousZone %userprofile%\desktop\tools\DangerousZone
 echo "Downloading SecLists..."
-git clone https://github.com/danielmiessler/SecLists %userprofile%\desktop\SecLists
+git clone https://github.com/danielmiessler/SecLists %userprofile%\desktop\tools\SecLists
